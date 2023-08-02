0b085f8554577fdb46da81b5ac64bbae35a8a1de
«Alyster» <AlysterKrouly@gmail.com>
2023-08-02 03:47:33 +0300

    в Examples003_Sum добавили рандомизацию чисел и вывод рандомных числе и их суммы для пользователяСумма чисел.

--- Examples/Examoles003_Sum/Program.cs
+++ Examples/Examoles003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random() .Next(1, 10); //случайное число от 1 до 10
+Console.Write ("Перове число:");
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1, 10); //случайное число от 1 до 10
+Console.Write ("Второе число:")  ; 
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write ("Сумма чисел");
 Console.WriteLine(result);
