2a12853c352a0b2e827b91a3670d6315810a837e
«Alyster» <AlysterKrouly@gmail.com>
2023-08-03 04:26:52 +0300

    Seminar1 добавлен и сделаны первые комментарии по пройденному материалу.

---
+++ Seminar1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Список стандартных операторов.
+int intNumber = 25; // Целые числа
+ double dobleNumber = 12.12; // Вещественные числа
+ string text = "some text"; // Текст
+ bool logicVar = true; // оператор условия +/- 
+*/
+
+
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine("My number are " + num1 + " and " + num2 + " and it is good!");
+Console.WriteLine($"My number are {num1} and {num2} and it is good!");
+Console.WriteLine("success");
+
+Console.WriteLine("input a number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
