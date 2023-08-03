bc29bf414aae39c43e83bf2f2a0d113736b167ad
«Alyster» <AlysterKrouly@gmail.com>
2023-08-03 04:46:33 +0300

    Seminar1 добавили 3 задачи

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -6,7 +6,7 @@ int intNumber = 25; // Целые числа
 */
 
 
-
+/* Список примеров с вводом и выводом чисел и текста 
 int num1 = 5;
 int num2 = 10;
 
@@ -18,3 +18,37 @@ Console.WriteLine("input a number");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Your number is " + num);
+*/
+
+//Task1. Напишите программу, которая на вход принимает два числа и проверяет,является ли первое число квадратом второго.
+/*
+Console.WriteLine("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+//Task2. Напишите программу которая на вход принимает положительное число (N), а на выводе показывает все целые числа в промежутке -N до N. 
+/*
+Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
